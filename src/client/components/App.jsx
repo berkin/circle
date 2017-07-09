088f9f4194ea01bf04dab7c2b07a243b733fb58d
berkin <berkin.cirak@comeon.com>
2017-07-09 13:11:54 +0300

    value check

--- src/client/components/App.jsx
+++ src/client/components/App.jsx
@@ -5,40 +5,45 @@ import '../styles/App.scss'
 import Post from './Post'
 import { changeRange, fetchArticle } from '../actions/'
 
-const App = ({ value, post, dispatch }) => (
-	<div className="container">
-		<div className="form-input">
-			<label htmlFor="selector" className="form-input__label">Enter post id:</label>
-			<input
-				id="selector"
-				className="selector"
-				type="number"
-				onChange={
-					(e) => {
-						dispatch(changeRange(e.target.value))
-					}
-				}
-			/>
-		</div>
-		<div className="circle-container">
-			<button
-				onClick={
-					() => {
-						dispatch(fetchArticle(value))
-					}
-				}
-				style={{
-					width: `${value}px`,
-					height: `${value}px`,
-					borderRadius: '100%'
-				}}
-				className="circle"
-			/>
-		</div>
-		<Post content={post} />
-	</div>
-)
+const App = ({ value, post, dispatch }) => {
+	const isValid = value >= 0 && value < 100
 
+	return (
+		<div className="container">
+			<div className="form-input">
+				<label htmlFor="selector" className="form-input__label">Enter post id:</label>
+				<input
+					id="selector"
+					className={isValid ? 'selector' : 'selector error'}
+					type="number"
+					onChange={
+						(e) => {
+							dispatch(changeRange(e.target.value))
+						}
+					}
+				/>
+			</div>
+			<div className="circle-container">
+				{isValid &&
+					<button
+						onClick={
+							() => {
+								dispatch(fetchArticle(value))
+							}
+						}
+						style={{
+							width: `${value}px`,
+							height: `${value}px`,
+						}}
+						className="circle"
+					/>
+				}
+			</div>
+			<Post content={post} />
+		</div>
+	)
+
+}
 const mapStateToProps = state => ({
 	value: parseInt(state.value, 10),
 	post: state.post,
